552765817b25f1d2548a6e5ea75a2134719c845c
LarissaGuder <lariguder10@gmail.com>
2020-11-17 19:23:24 -0300

    Adicionado  react  app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
